759ce6d643041e8ba48d2f78689afff270abdf8e
Randy DuCharme <radio.ad5gb@gmail.com>
2015-04-15 18:41:12 -0500

    Fix makefile
    Signed-off-by: Randy DuCharme <radio.ad5gb@gmail.com>

--- validus.mk
+++ validus.mk
@@ -18,7 +18,7 @@ $(call inherit-product, device/asus/deb/full_deb.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := deb
-PRODUCT_NAME := cm_deb
+PRODUCT_NAME := validus_deb
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
