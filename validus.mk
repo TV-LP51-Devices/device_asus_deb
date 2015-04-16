2e270859eec8e07f42ddf97dbd0b8be8db1e1162
Randy DuCharme <radio.ad5gb@gmail.com>
2015-04-15 21:56:25 -0500

    Disable half-res bootani
    Signed-off-by: Randy DuCharme <radio.ad5gb@gmail.com>

--- validus.mk
+++ validus.mk
@@ -1,8 +1,8 @@
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1200
-TARGET_BOOTANIMATION_HALF_RES := true
+#TARGET_SCREEN_HEIGHT := 1920
+#TARGET_SCREEN_WIDTH := 1200
+#TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/validus/config/common_full_tablet_wifionly.mk)
